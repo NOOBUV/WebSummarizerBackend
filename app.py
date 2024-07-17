759e90fb7a8083f1c3c62686bf7946bab7ed64e8
Utkarsh Vijay <utkarshvijay99@gmail.com>
2024-07-17 08:03:50 +0530

    adding files for deployment

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import os
 import tempfile
+import stat
 from webCrawler import WebCrawler
 from summarizer import summarize_content
 
-tempfile.tempdir = './temp'
+temp_dir = './tmp'
+
+# Ensure the directory exists
+os.makedirs(temp_dir, exist_ok=True)
+
+# Set full permissions for the user (read, write, execute)
+os.chmod(temp_dir, stat.S_IRWXU)
+
+# Set the custom temporary directory
+tempfile.tempdir = temp_dir
 app = FastAPI()
 
 
